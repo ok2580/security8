1cd4bbda05557327ec40780f6ba9cc531657e7f9
e1b22001 <e1b22001@oit.ac.jp>
2024-10-08 14:42:12 +0900

    認証処理を実装

---
+++ src/main/java/oit/is/team_8/security8/security/Sample3AuthConfiguration.java
@@ -0,0 +1,45 @@
+package oit.is.team_8.security8.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration {
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   *
+   * @param http
+   * @return
+   * @throws Exception
+   */
+
+  /**
+   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
+   *
+   * @return
+   */
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    // ユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
+    // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
+
+    UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$05$MDc6fklneBhj.CfdZ92gK.fuCVzsFdX/rK9.YjQTY2fjJI7IaHv6W").roles("USER").build();
+    UserDetails admin = User.withUsername("admin")
+        .password("{bcrypt}$2y$05$y.1qL5Bf3RW3XjEmseUQI.YIPSi27Gta0OVxY3QXb2KlOj4SPibqK").roles("ADMIN").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(user1, admin);
+  }
+
+}
