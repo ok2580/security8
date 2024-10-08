34a0fd27191b020a105a62afaf98f74cec999d39
ok2258 <gomo20031119@gmail.com>
2024-10-08 13:55:41 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team_8/security8/Security8ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team_8.security8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
