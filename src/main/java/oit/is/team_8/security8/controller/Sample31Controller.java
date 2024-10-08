e42b14d98fc218edfbba2f0fea561df789cdde54
e1b22031kishimura <e1b22031@oit.ac.jp>
2024-10-08 15:07:58 +0900

    GETリクエストを実装する

--- src/main/java/oit/is/team_8/security8/controller/Sample31Controller.java
+++ src/main/java/oit/is/team_8/security8/controller/Sample31Controller.java
@@ -1,3 +1,22 @@
+package oit.is.team_8.security8.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/sample3")
+
 public class Sample31Controller {
-  
+
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
 }
