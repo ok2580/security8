870c76660c190d1b2789928d8f52f2ea846991c4
e1b22031kishimura <e1b22031@oit.ac.jp>
2024-10-08 16:20:47 +0900

    入室処理をDIとnew2種類の方法で実装する

--- src/main/java/oit/is/team_8/security8/controller/Sample31Controller.java
+++ src/main/java/oit/is/team_8/security8/controller/Sample31Controller.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.team_8.security8.model.Room;
+
 @Controller
 @RequestMapping("/sample3")
 
 public class Sample31Controller {
+
+  @Autowired
+  private Room room;
 
   @GetMapping("step1")
   public String sample31() {
@@ -42,5 +47,24 @@ public class Sample31Controller {
     int kekka = hiku1 - hiku2;
     model.addAttribute("hikukekka", kekka);
     return "sample33.html";
+  }
+
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "sample37.html";
+  }
+
+  @GetMapping("step9")
+  public String sample39(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Room newRoom = new Room();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
+
+    return "sample37.html";
   }
 }
